57b308f59eaf717991dd24c2c1aafc5e89b3c310
CalebTelfer <114375918+CalebTelfer@users.noreply.github.com>
2025-06-18 19:53:29 -0400

    populate board grid boxes with divs in JS

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import {Ship, Gameboard, Player} from "./shipBoardPlayer.js";
 // Main event listener file. drive the game from here.
 
 //create a module that helps amanage actions that should happen in the dom.
+
+//populate grid w javascript. not making 100 squares manually
+const playerBoard = document.querySelector(".player-board");
+const cpuBoard = document.querySelector(".cpu-board");
+
+for (let i = 0; i < 100; i++) {
+    let playerSquare = document.createElement("div");
+    let cpuSquare = document.createElement("div");
+
+    playerBoard.appendChild(playerSquare);
+    cpuBoard.appendChild(cpuSquare);
+}
