983d0114a26f4368e37ff2dee5b32859a9a4478a
CalebTelfer <114375918+CalebTelfer@users.noreply.github.com>
2025-06-18 23:34:37 -0400

    WIP: push from work pc

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import {Ship, Gameboard, Player} from "./shipBoardPlayer.js";
 
 // Main event listener file. drive the game from here.
 
-//create a module that helps amanage actions that should happen in the dom.
+//GAME STATES
+// 0 - not started, 1- placing Ship, 2- firing at board
+let gameState = 0;
 
 //populate grid w javascript. not making 100 squares manually
 const playerBoard = document.querySelector(".player-board");
@@ -12,6 +14,32 @@ for (let i = 0; i < 100; i++) {
     let playerSquare = document.createElement("div");
     let cpuSquare = document.createElement("div");
 
+    playerSquare.addEventListener("click", (e) => {
+        switch (gameState) {
+            case 0: // game not started
+                break;
+
+            case 1: // awaiting ship placement
+
+                break;
+
+            case 2: //firing at board.
+                
+                break;
+        
+            default:
+                break;
+        }
+    });
+
     playerBoard.appendChild(playerSquare);
     cpuBoard.appendChild(cpuSquare);
 }
+
+
+const startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", (e) => {
+    //Logic to begin user input for placement.
+    //DOM manipulation: remove this button -> append select / place buttons.
+})
