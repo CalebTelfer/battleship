104a67a66736489b38067fb42b69500646c0448a
caleb <calebtelfe@gmail.com>
2025-07-09 12:51:20 -0400

    initboard moved files. gameState object added

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,100 @@
+import { gameState } from ".";
+
 export function numberToLetter(n) {
     if (n >= 0 && n < 26) {
       return String.fromCharCode(97 + n); // 97 is 'a'
     }
+}
+
+export function initBoardSquares() {
+  //populate grid w javascript. not making 100 squares manually
+const playerBoard = document.querySelector(".player-board");
+const cpuBoard = document.querySelector(".cpu-board");
+
+for (let i = 0; i < 121; i++) {
+    let playerSquare = document.createElement("div");
+    let cpuSquare = document.createElement("div");
+
+    if (i % 11 == 0 && i != 0) { // Letter axis
+        cpuSquare.style.backgroundColor = "blue";
+        let letter = numberToLetter((i/11) - 1); // each letter square is a multiple of 11. so the 3rd one is 33/11. = 3.
+        let letterText = document.createElement("h3");
+        letterText.textContent = letter;
+        cpuSquare.appendChild(letterText);
+
+        playerSquare.style.backgroundColor = "blue";
+        let playerletter = numberToLetter((i/11) - 1); // each letter square is a multiple of 11. so the 3rd one is 33/11. = 3.
+        let playerletterText = document.createElement("h3");
+        playerletterText.textContent = playerletter;
+        playerSquare.appendChild(playerletterText);
+    }
+
+    if (i < 11 && i!= 0) {  // number axis
+        cpuSquare.style.backgroundColor = "blue";
+        let numberText = document.createElement("h3");
+        numberText.textContent = i;
+        cpuSquare.appendChild(numberText);
+
+
+        playerSquare.style.backgroundColor = "blue";
+        let playernumberText = document.createElement("h3");
+        playernumberText.textContent = i;
+        playerSquare.appendChild(playernumberText);
+    }
+
+    playerSquare.addEventListener("click", (e) => {
+        if (!gameState.gameStarted) {
+          return;
+        }
+
+        if (!gameState.playerMove) {
+          if(gameState.cpuPlacingShip) {
+            //cpu trying to place ship on wrong board.
+            return;
+          }
+
+          if(gameState.cpuFiring) {
+            //CPU firing at this square.
+          }
+        }
+
+
+        if (gameState.playerMove) {
+          if (gameState.playerPlacingShip) {
+            //place player ship
+          } else {
+            return; // else firing at own board
+          }
+        }
+    });
+
+    cpuSquare.addEventListener("click", (e) => {
+      if (!gameState.gameStarted) {
+        return;
+      }
+
+      if (!gameState.cpuMove) {
+        if(gameState.playerPlacingShip) {
+          //player trying to place ship on wrong board.
+          return;
+        }
+
+        if(gameState.playerFiring) {
+          //player firing at this square.
+        }
+      }
+
+
+      if (gameState.cpuMove) {
+        if (gameState.cpuPlacingShip) {
+          //place cpu ship
+        } else {
+          return; // else firing at own board
+        }
+      }
+    });
+
+    playerBoard.appendChild(playerSquare);
+    cpuBoard.appendChild(cpuSquare);
+}
 }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
 import {Ship, Gameboard, Player} from "./shipBoardPlayer.js";
 import "./styles.css";
-import { numberToLetter } from "./functions.js";
+import { initBoardSquares, numberToLetter } from "./functions.js";
 
 // Main event listener file. drive the game from here.
 
 //GAME STATES
-// 0 - not started, 1- placing Ship, 2- firing at board 
-let gameState = 0;
+export const gameState = {
+    gameStarted: false,
 
-//populate grid w javascript. not making 100 squares manually
-const playerBoard = document.querySelector(".player-board");
-const cpuBoard = document.querySelector(".cpu-board");
+    playerMove: false,
+    cpuMove: false,
 
-for (let i = 0; i < 121; i++) {
-    let playerSquare = document.createElement("div");
-    let cpuSquare = document.createElement("div");
+    playerPlacingShip: false,
+    cpuPlacingShip: false,
 
-    if (i % 11 == 0 && i != 0) { // Letter axis
-        cpuSquare.style.backgroundColor = "blue";
-        let letter = numberToLetter((i/11) - 1); // each letter square is a multiple of 11. so the 3rd one is 33/11. = 3.
-        let letterText = document.createElement("h3");
-        letterText.textContent = letter;
-        cpuSquare.appendChild(letterText);
-    }
-
-    if (i < 11 && i!= 0) {  // number axis
-        cpuSquare.style.backgroundColor = "blue";
-        let numberText = document.createElement("h3");
-        numberText.textContent = i;
-        cpuSquare.appendChild(numberText);
-    }
-
-    playerSquare.addEventListener("click", (e) => {
-        switch (gameState) {
-            case 0: // game not started
-                break;
-
-            case 1: // awaiting ship placement
-
-                break;
-
-            case 2: //firing at board.
-                
-                break;
-        
-            default:
-                break;
-        }
-    });
-
-    playerBoard.appendChild(playerSquare);
-    cpuBoard.appendChild(cpuSquare);
+    playerFiring: false,
+    cpuFiring: false
 }
 
+initBoardSquares();
 
 const startButton = document.querySelector("#start-button");
 
